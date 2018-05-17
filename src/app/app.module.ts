76c7880871bdbb00dd94bd6eaca7f8697c23ac99
susanacadenas81 <susanacadenasgomez81@gmail.com>
2018-05-17 16:35:13 +0200

    creación del navbar y de la Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
